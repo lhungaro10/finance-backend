659c4f091b7eec7e3066c453765f8d9acbe8ea56
Lucas Hungaro <lhungaro10@gmail.com>
2025-08-03 22:39:55 -0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.listen({ port: 8080 }, () => {
+	console.log(`listening on http://localhost:8080/`);
+});
